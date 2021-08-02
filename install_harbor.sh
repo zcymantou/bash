f3d8e678108dff3a3f1b8ba1c14af2367ea27d13
zcymantou <2858349172@qq.com>
2021-08-02 11:20:38 +0800

    安装docker-compose脚本修改

--- install_harbor.sh
+++ install_harbor.sh
@@ -14,8 +14,7 @@ bash /opt/scripts/docker_install.sh
 
 echo "#################开始安装docker-compose环境################"
 wget https://github.com/docker/compose/releases/download/1.28.6/docker-compose-Linux-x86_64
-cp docker-compose-Linux-x86_64 /usr/local/bin/docker-compose 
-
+cp docker-compose-Linux-x86_64 /usr/local/bin/docker-compose && chmod o+x /usr/local/bin/docker-compose
 
 docker-compose -v
 if [ $? -eq 0 ]
